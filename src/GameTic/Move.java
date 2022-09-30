b65838a10f3d339fac023c4364bb58e15f8c8deb
Fernando <lfernnado.adriano@gmail.com>
2022-09-30 16:49:12 -0300

    new class Move

---
+++ src/GameTic/Move.java
@@ -0,0 +1,22 @@
+package GameTic;
+
+public class Move {
+    private int line;
+    private int column;
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
